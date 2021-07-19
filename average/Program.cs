e3318c1e5a067b3a808fa419b0b2ed7662824614
manish-1498 <manishpradhan14121998@gmail.com>
2021-07-19 22:21:54 +0530

    [manish]add:average_program

---
+++ average/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how many numbers do you have");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int average;
+            for (int i = 1; i <=n; i++)
+            {
+                Console.WriteLine("enter" + i + "number");
+                int number=Convert.ToInt32(Console.ReadLine());
+                sum += number;            
+            }
+            average = sum / n;
+            Console.WriteLine(average);
+        }
+    }
+}
